7ecc1f48421ffe0b09a23c944217132d55d9c80d
arturzinin <a.zinin@goit.ua>
2024-10-15 12:00:19 +0300

    update gradle deps

---
+++ rollup.config.mjs
@@ -0,0 +1,48 @@
+import typescript from 'rollup-plugin-ts';
+import json from '@rollup/plugin-json';
+import { folderInput } from 'rollup-plugin-folder-input';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: [
+    'src/**/*.ts*',
+  ],
+  output: {
+    dir: 'lib',
+    format: 'es',
+    sourcemap: true,
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    exports: 'auto',
+  },
+  external: [
+    'react',
+    'react-native',
+  ],
+  plugins: [
+    folderInput(),
+    typescript({
+      tsconfig: resolvedConfig => ({
+        ...resolvedConfig,
+        declaration: true,
+        importHelpers: true,
+        plugins: [
+          {
+            "transform": "@zerollup/ts-transform-paths",
+            "exclude": ["*"]
+          }
+        ]
+      }),
+    }),
+    json(),
+    copy({
+      targets: [
+        { src: 'ios/**/*', dest: 'lib/android' },
+        { src: 'android/**/*', dest: 'lib/ios' },
+        { src: 'package.json', dest: 'lib' },
+        { src: 'README.md', dest: 'lib' },
+        { src: 'RNAppleMusic.podspec', dest: 'lib'},
+      ]
+    })
+  ],
+};
